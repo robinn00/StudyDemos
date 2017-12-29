806e499d0248c0b87317796892581be40f051bb7
robinn00 <robinn00@163.com>
2017-12-29 09:25:33 +0800

    Bootstrap demos

---
+++ Bootstrap/tsdemo.js
@@ -0,0 +1,11 @@
+/**
+ * Created by admin on 2016/10/7.
+ */
+function ge(person) {
+return "types" + person;
+}
+var person = "userinfo";
+document.body.innerHTML = ge(person);
+var lists = ['12', '23'];
+document.body.innerText = lists.toString();
+//# sourceMappingURL=tsdemo.js.map
---
+++ Bootstrap/tsdemo.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 2016/10/7.
+ */
+function ge(person:string){
+    return "types"+person;
+}
+var person = "userinfo";
+document.body.innerHTML = ge(person);
+var lists = ['12','23'];
+document.body.innerText = lists.toString();
